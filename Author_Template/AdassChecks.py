306e1f9b7dda0570095d39c2961b63ab74d541e3
Peter Teuben <teuben@gmail.com>
2018-10-18 12:10:38 -0400

    editors corrected for 2018

--- Author_Template/AdassChecks.py
+++ Author_Template/AdassChecks.py
@@ -278,12 +278,11 @@ import TexScanner
 #  used by the various utility scripts used in the editing process for the
 #  proceedings.)
 
-__AdassConference__ = "XXVI"
+__AdassConference__ = "XXVIII"
 
-__AdassEditors__ = \
-             "Pasian,~F. and Molinaro,~M. and Mansutti,~O. and Shortridge,~K."
+__AdassEditors__    = "Teuben,~P. and Pound,~M. and Thomas,~B. and Warner,~E."
 
-__AdassVolume__ = "TBD"
+__AdassVolume__     = "TBD"
 
 # ------------------------------------------------------------------------------
 
