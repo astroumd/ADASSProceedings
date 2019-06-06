9b68880dc24a2ed26c5046ef39eae50021b073ac
Peter Teuben <teuben@gmail.com>
2019-06-06 16:08:16 -0400

    remove debug

--- Author_Template/tex2inc.py
+++ Author_Template/tex2inc.py
@@ -25,7 +25,7 @@ import sys
 
 version = "6-jun-2019"
 debug = False
-debug = True                              # comment this for final version, or read from a dot file
+##debug = True                              # %%DEBUG comment this line for final version, or read from a dot file
 
 def read1(filename):
     """ read tex file into lines for processing
